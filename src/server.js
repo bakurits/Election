7b9176831d905228954c36871b9f18ca396b0149
bakurits <bakuricucxashvili@gmail.com>
2018-12-25 15:00:39 +0400

    connected to Ganache

--- src/server.js
+++ src/server.js
@@ -1,7 +1,16 @@
 const express = require('express')
-const web3 = require('web3')
+
+const Web3 = require('web3')
+
+if (typeof web3 !== 'undefined') {
+    web3 = new Web3(web3.currentProvider);
+} else {
+    // Set the provider you want from Web3.providers
+    web3 = new Web3(new Web3.providers.HttpProvider("HTTP://127.0.0.1:7545"));
+}
 const app = express()
-const port = 3000
+const port = 3001
+
 
 app.get('/', (req, res) => res.sendFile( __dirname + '/index.html'))
 
