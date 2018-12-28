81ddbee6b08b34324bf1f55c51906e0d329323e6
bakurits <bakuricucxashvili@gmail.com>
2018-12-28 17:29:50 +0400

    Some chages

--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
-const express = require('express')
-const fs = require('fs');
-var ipfsClient = require('ipfs-http-client')
+import express from 'express';
+import { readFileSync } from 'fs';
 const app = express()
 const port = 3000
 app.get('/', function (req, res) {
@@ -13,7 +12,7 @@ app.get('/web3.js', function (req, res) {
     res.sendFile(__dirname + '/js/web3.min.js');
 });
 app.get('/smart-contract-info', function (req, res) {
-    var obj = JSON.parse(fs.readFileSync('build/contracts/Voting.json', 'utf8'));
+    var obj = JSON.parse(readFileSync('build/contracts/Voting.json', 'utf8'));
 
     let abi = obj.abi;
 
